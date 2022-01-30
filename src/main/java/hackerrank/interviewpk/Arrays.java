bba6b14ceca142f739cefbd11769881ada9ae574
Alvaro J Garcia <aj.garciac@gmail.com>
2022-01-29 20:53:30 -0500

    Second commit

---
+++ src/main/java/hackerrank/interviewpk/Arrays.java
@@ -0,0 +1,16 @@
+package hackerrank.interviewpk;
+
+import java.util.List;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void minimumBribes(List<Integer> q) {
+
+
+    }
+
+}
